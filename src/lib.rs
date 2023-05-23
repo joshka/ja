3fafc690f2e9c3d6cfbb7f1999f69ff2e56edb15
Josh McKinney <joshka@users.noreply.github.com>
2023-05-23 04:13:29 -0700

    feat: interactive mode, multiple commands, tracing
    Refactor and large changes towards multiple commands, tracing,
    interactive chat, more cli args, etc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,106 @@
-use clap::{Parser, ValueEnum};
-use std::fmt::Debug;
+use std::path::PathBuf;
 
-#[derive(Parser, Debug)]
+use async_openai::types::CreateChatCompletionRequestArgs;
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
+#[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    #[arg(long, short = 'n', default_value = "1000")]
-    pub max_tokens: usize,
+    #[command(subcommand)]
+    pub command: Option<Command>,
 
-    #[arg(long, short, default_value_t = Model::Gpt35)]
-    pub model: Model,
+    #[command(flatten)]
+    pub chat: ChatCompletionArgs,
 
-    #[arg(long, short, default_value = "0.7")]
-    pub temperature: f32,
-
-    #[arg(long, short)]
+    /// Show verbose output
+    #[arg(long, short = 'v')]
     pub verbose: bool,
+}
 
-    #[arg(trailing_var_arg = true)]
-    pub input: Vec<String>,
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// default command (can be omitted)
+    Chat(ChatCompletionArgs),
+    Logs,
+}
+
+#[derive(Debug, Args)]
+pub struct ChatCompletionArgs {
+    /// ID of the model to use
+    #[arg(long, short = 'm', default_value = "gpt-3.5-turbo")]
+    model: Option<Model>,
+
+    /// Sampling temperature
+    #[arg(long, short = 't', default_value = "0.7")]
+    temperature: Option<f32>,
+
+    /// Probability mass percentage
+    #[arg(long, short = 'p', default_value = "1.0")]
+    top_p: Option<f32>,
+
+    /// Number of choices to generate for each input message
+    #[arg(long, short, default_value = "1")]
+    n: Option<u8>,
+
+    /// Stream the resultt of the API call
+    #[arg(long, short, default_value = "true")]
+    stream: Option<bool>,
+
+    /// Up to 4 sequences where the API will stop generating further tokens
+    #[arg(long)]
+    stop: Option<Vec<String>>,
+
+    /// The maximum number of tokens to generate in the chat completion
+    #[arg(long, short = 'c')]
+    max_tokens: Option<u16>,
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they
+    /// appear in the text so far
+    #[arg(long, default_value = "0.0")]
+    presence_penalty: Option<f32>,
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing
+    /// frequency in the text so far
+    #[arg(long, default_value = "0.0")]
+    frequency_penalty: Option<f32>,
+
+    /// A unique identifier representing your end-user, which can help OpenAI to monitor and detect
+    /// abuse.
+    #[arg(long, short = 'u')]
+    user: Option<String>,
+
+    /// Input file
+    #[arg(long, short = 'i')]
+    input: Option<PathBuf>,
+
+    /// Output file
+    #[arg(long, short = 'o')]
+    output: Option<PathBuf>,
+
+    /// System message
+    #[arg(long)]
+    pub system: Option<String>,
+
+    #[arg(last(true))]
+    pub message: Option<Vec<String>>,
+}
+
+#[allow(dead_code)]
+impl ChatCompletionArgs {
+    pub fn to_chat(&self) -> CreateChatCompletionRequestArgs {
+        let mut builder = CreateChatCompletionRequestArgs::default();
+        self.model.map(|m| builder.model(m.to_string()));
+        self.temperature.map(|t| builder.temperature(t));
+        self.top_p.map(|p| builder.top_p(p));
+        self.n.map(|n| builder.n(n));
+        self.stream.map(|s| builder.stream(s));
+        self.stop.as_ref().map(|s| builder.stop(s));
+        self.max_tokens.map(|m| builder.max_tokens(m));
+        self.presence_penalty.map(|p| builder.presence_penalty(p));
+        self.frequency_penalty.map(|p| builder.frequency_penalty(p));
+        self.user.as_ref().map(|u| builder.user(u));
+        builder
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -26,6 +109,7 @@ pub enum Model {
     /// alias 3.5
     #[value(name = "gpt-3.5-turbo", alias = "3.5")]
     Gpt35,
+
     /// alias 4
     #[value(name = "gpt-4", alias = "4")]
     Gpt4,
--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,212 @@
 use anyhow::{Context, Result};
 use async_openai::{
-    types::{
-        ChatChoice, ChatChoiceDelta, ChatCompletionRequestMessageArgs,
-        ChatCompletionResponseMessage, CreateChatCompletionRequest,
-        CreateChatCompletionRequestArgs, CreateChatCompletionResponse,
-        CreateChatCompletionStreamResponse,
-    },
+    types::{ChatCompletionRequestMessageArgs, Role},
     Client,
 };
 use atty::Stream;
 use clap::Parser;
-use derive_builder::Builder;
-use directories::ProjectDirs;
 use futures::StreamExt;
-use ja::{Cli, Model};
-use serde::{Deserialize, Serialize};
-use std::{
-    fmt::Debug,
-    fs::{create_dir_all, File},
-    io::{stderr, stdin, stdout, Read, Write},
-    path::PathBuf,
-    process::exit,
-};
-use time::format_description::well_known::Iso8601;
-use time::OffsetDateTime;
-
-#[derive(Clone, Serialize, Debug, Builder, Deserialize, PartialEq)]
-struct Interaction {
-    request: CreateChatCompletionRequest,
-    response: CreateChatCompletionResponse,
-}
-
-fn from_chat_stream(value: CreateChatCompletionStreamResponse) -> CreateChatCompletionResponse {
-    CreateChatCompletionResponse {
-        id: value.id.unwrap(),
-        object: value.object,
-        created: value.created,
-        model: value.model,
-        choices: value.choices.into_iter().map(from_choice_delta).collect(),
-        usage: value.usage,
-    }
-}
-
-fn from_choice_delta(value: ChatChoiceDelta) -> ChatChoice {
-    let message = ChatCompletionResponseMessage {
-        content: String::default(),
-        role: value.delta.role.unwrap_or_default(),
-    };
-    ChatChoice {
-        index: value.index,
-        message,
-        finish_reason: value.finish_reason,
-    }
-}
+use ja::{ChatCompletionArgs, Cli, Command};
+use std::io::Write;
+use tracing::{info, metadata::LevelFilter, warn};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
-    let time_stamp = OffsetDateTime::now_utc().format(&Iso8601::DEFAULT)?;
-    let cache_dir = ensure_cache_dir_exists()?;
-
-    let path = cache_dir.join(format!("{time_stamp}.json"));
-    if cli.verbose {
-        writeln!(stderr(), "writing to {path:?}")?;
+    init_tracing(&cli);
+    info!("cli: {:?}", cli);
+    let command = cli.command.unwrap_or(Command::Chat(cli.chat));
+    match command {
+        Command::Chat(args) => chat(args).await?,
+        _ => warn!("unimplemented command"),
     }
-    let mut stdout = stdout().lock();
 
-    let input = get_input(&cli, &mut stdout)?;
-    if input.trim().is_empty() {
-        writeln!(stderr(), "no input")?;
-        exit(1);
+    Ok(())
+}
+
+fn init_tracing(cli: &Cli) {
+    let level = if cli.verbose {
+        LevelFilter::INFO
+    } else {
+        LevelFilter::WARN
+    };
+    tracing_subscriber::fmt().with_max_level(level).init();
+}
+
+// TODO refactor this
+async fn chat(args: ChatCompletionArgs) -> Result<()> {
+    match InputMode::from(&args) {
+        InputMode::Cli(message) => cli_mode(message, args).await?,
+        InputMode::Pipe => todo!(),
+        InputMode::Interactive => interactive_mode(&args).await?,
     }
-    let request = create_request(cli.model, cli.temperature, input)?;
-    let mut stream = Client::new().chat().create_stream(request.clone()).await?;
-    let mut response = None;
-    while let Some(result) = stream.next().await {
-        match result {
-            Ok(stream_response) => {
-                let response =
-                    response.get_or_insert_with(|| from_chat_stream(stream_response.clone()));
-                let stream_choice = stream_response.choices.get(0).unwrap();
-                let mut response_choice = response.choices.get_mut(0).unwrap();
-                if let Some(ref content) = stream_choice.delta.content {
-                    write!(stdout, "{}", content)?;
-                    response_choice.message.content.push_str(content);
-                }
-                if let Some(ref role) = stream_choice.delta.role {
-                    write!(stdout, "{}: ", role)?;
-                    response_choice.message.role = role.clone();
-                }
+    Ok(())
+}
+
+use dialoguer::{theme::ColorfulTheme, Input, Select};
+
+const CODE_OPTIMIZED: &str = "You are an AI programming assistant.
+Follow the user's requirements carefully & to the letter.
+Your responses must not be accusing, rude, controversial or defensive.
+Your responses should be informative and logical.
+You should always adhere to technical information.
+If the user asks for code or technical questions, you must provide code suggestions and adhere to technical information.
+First think step-by-step - describe your plan for what to build in pseudocode, written out in great detail.
+Then output the code in a single code block.
+Minimize any other prose.
+Keep your answers short and impersonal.
+Use Markdown formatting in your answers.
+Make sure to include the programming language name at the start of the Markdown code blocks.
+Avoid wrapping the whole response in triple backticks.
+The user works in an IDE called Visual Studio Code which has a concept for editors with open files, integrated unit test support, an output pane that shows the output of running the code as well as an integrated terminal.
+The active document is the source code the user is looking at right now.
+You can only give one reply for each conversation turn.
+You should always generate short suggestions for the next user turns that are relevant to the conversation and not offensive.
+";
+
+async fn interactive_mode(args: &ChatCompletionArgs) -> Result<()> {
+    let mut stderr = std::io::stderr();
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .item("None")
+        .item("Code optimized")
+        .item("Custom")
+        .default(0)
+        .with_prompt("System prompt")
+        .interact_opt()?;
+    let system_prompt = match selection {
+        None => None,
+        Some(0) => None,
+        Some(1) => Some(CODE_OPTIMIZED.to_string()),
+        Some(2) => {
+            let input = Input::<String>::with_theme(&ColorfulTheme::default())
+                .with_prompt("Custom Prompt")
+                .with_initial_text("You are a helpful assistant.")
+                .interact_text()?;
+            Some(input)
+        }
+        Some(_) => unreachable!(),
+    };
+    let mut messages = vec![];
+    if let Some(system) = &system_prompt {
+        let message = ChatCompletionRequestMessageArgs::default()
+            .content(system)
+            .role(Role::System)
+            .build()
+            .context("system")?;
+        messages.push(message);
+    }
+    loop {
+        let user_input = Input::<String>::with_theme(&ColorfulTheme::default())
+            .with_prompt("User Prompt (type exit to exit)")
+            .default("exit".into())
+            .with_post_completion_text("User Prompt")
+            .interact_text()?;
+        if user_input == "exit" {
+            break;
+        }
+        let message = ChatCompletionRequestMessageArgs::default()
+            .content(user_input)
+            .role(Role::User)
+            .build()?;
+        messages.push(message);
+        let request = args
+            .to_chat()
+            .messages(messages.clone())
+            .build()
+            .context("request")?;
+        let mut response = Client::new().chat().create_stream(request).await?;
+        let mut role = Role::Assistant;
+        let mut content = String::new();
+        while let Some(response) = response.next().await {
+            let response = response?;
+            let choice = response.choices.get(0).unwrap();
+            if let Some(r) = &choice.delta.role {
+                writeln!(stderr, "{}:", role)?;
+                role = r.clone();
+                stderr.flush()?;
             }
-            Err(err) => {
-                writeln!(stdout, "error: {err}")?;
+            if let Some(token) = &choice.delta.content {
+                write!(stderr, "{}", token)?;
+                content.push_str(token);
+                stderr.flush()?;
             }
         }
-        stdout.flush()?;
+        writeln!(stderr)?;
+        stderr.flush()?;
+        let message = ChatCompletionRequestMessageArgs::default()
+            .role(role)
+            .content(content)
+            .build()
+            .unwrap();
+        messages.push(message);
     }
+    Ok(())
+}
 
-    let interaction = Interaction {
-        request,
-        response: response.unwrap(),
-    };
-    let mut file = File::create(path).context("create response file")?;
-    serde_json::to_writer_pretty(&file, &interaction).context("writing file")?;
-    file.flush().context("flush")?;
-
+async fn cli_mode(message: String, args: ChatCompletionArgs) -> Result<()> {
+    let mut stdout = std::io::stdout();
+    info!("message: {}", message);
+    let mut messages = vec![];
+    args.system.as_ref().map(|system| {
+        let message = ChatCompletionRequestMessageArgs::default()
+            .content(system)
+            .role(Role::System)
+            .build()
+            .context("system")
+            .unwrap();
+        messages.push(message);
+    });
+    messages.push(
+        ChatCompletionRequestMessageArgs::default()
+            .content(message)
+            .build()
+            .context("message")?,
+    );
+    let request = args
+        .to_chat()
+        .messages(messages)
+        .build()
+        .context("request")?;
+    info!("request: {:?}", request);
+    let mut stream = Client::new().chat().create_stream(request).await?;
+    while let Some(response) = stream.next().await {
+        match response {
+            Ok(response) => {
+                let choice = response.choices.get(0).unwrap();
+                if let Some(role) = &choice.delta.role {
+                    writeln!(stdout, "{}:", role)?;
+                    stdout.flush()?;
+                }
+                if let Some(content) = &choice.delta.content {
+                    write!(stdout, "{}", content)?;
+                    stdout.flush()?;
+                }
+                // TODO markdown / syntax
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+            }
+        }
+    }
     writeln!(stdout)?;
     stdout.flush()?;
     Ok(())
 }
 
-fn get_input(cli: &Cli, stdout: &mut impl Write) -> Result<String> {
-    if !cli.input.is_empty() {
-        Ok(cli.input.join(" "))
-    } else if atty::is(Stream::Stdin) {
-        writeln!(stdout, "Enter your input (finish with ^D): ")?;
-        let mut buf = String::new();
-        stdin()
-            .lock()
-            .read_to_string(&mut buf)
-            .context("reading stdin")?;
-        Ok(buf)
-    } else {
-        let mut buf = String::new();
-        stdin()
-            .lock()
-            .read_to_string(&mut buf)
-            .context("reading stdin from pipe")?;
-        Ok(buf)
-    }
-}
-fn ensure_cache_dir_exists() -> Result<PathBuf> {
-    let projects = ProjectDirs::from("net", "joshka", "ja").context("No valid home directory")?;
-    let cache_dir = projects.cache_dir();
-    create_dir_all(cache_dir).context("creating cache dir")?;
-    Ok(cache_dir.to_path_buf())
+enum InputMode {
+    Cli(String),
+    Pipe,
+    Interactive,
 }
 
-fn create_request(
-    model: Model,
-    temperature: f32,
-    content: String,
-) -> Result<CreateChatCompletionRequest> {
-    let message = ChatCompletionRequestMessageArgs::default()
-        .content(content)
-        .build()
-        .context("message")?;
-    CreateChatCompletionRequestArgs::default()
-        .model(model.to_string())
-        .messages(vec![message])
-        .temperature(temperature)
-        .build()
-        .context("request")
+impl InputMode {
+    fn from(args: &ChatCompletionArgs) -> Self {
+        if let Some(ref message) = args.message {
+            InputMode::Cli(message.join(" "))
+        } else if atty::is(Stream::Stdin) {
+            InputMode::Interactive
+        } else {
+            InputMode::Pipe
+        }
+    }
 }
